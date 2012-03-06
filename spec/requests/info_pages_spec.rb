1e87c788de14f8022aae57cab053bb26793c04b5
PunkWare <punkware@free.fr>
2012-03-06 22:12:42 +0100

    Write integration tests for info_pages

--- spec/requests/info_pages_spec.rb
+++ spec/requests/info_pages_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
-describe "InfoPages" do
-  describe "GET /info_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get info_pages_index_path
-      response.status.should be(200)
+describe "info_pages" do
+  describe "home page" do
+    it "should have the content 'Backlog Blooming'" do
+      visit '/info_pages/home'
+      page.should have_content('Backlog Blooming')
+    end
+  end
+  
+  describe "help page" do
+    it "should have the content 'Backlog Blooming'" do
+      visit '/info_pages/help'
+      page.should have_content('Backlog Blooming')
     end
   end
 end
