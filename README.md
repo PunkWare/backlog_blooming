7de14caf0e4b9aecfa6508b28173e603fb459efe
PunkWare <punkware@free.fr>
2012-03-06 21:07:22 +0100

    Project start

---
+++ README.md
@@ -0,0 +1,8 @@
+Backlog Blooming
+	- Sprout your user stories
+	- Nourish your sprints
+	- Hatch your planning
+	- Grow your confidence
+	- Harvest success
+
+by [PunkWare] (https://github.com/PunkWare).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BacklogBlooming::Application.config.secret_token = 'ab95807555ec1a9a6e03ed3c806bfd5d44393ba65943fcb81c9845a2f622e312e8302b30d1dde32fec891bb6cf9142e910310a64cd4f09420f56145d4190f40f'
