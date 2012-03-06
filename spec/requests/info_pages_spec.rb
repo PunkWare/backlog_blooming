74b2420af8737cddc77335599e9635e7b27a3ed8
PunkWare <punkware@free.fr>
2012-03-06 22:51:40 +0100

    Done with home, help and about pages

--- spec/requests/info_pages_spec.rb
+++ spec/requests/info_pages_spec.rb
@@ -1,24 +1,41 @@
 require 'spec_helper'
 
 describe "info_pages" do
+  
   describe "home page" do
-    it "should have the content 'Backlog Blooming | Home'" do
+    it "should have the right title" do
       visit '/info_pages/home'
-      page.should have_content('Backlog Blooming | Home')
+      page.should have_selector('title', :text => 'Backlog Blooming | Home')
+    end
+      
+    it "should have the right h1" do
+      visit '/info_pages/home'
+      page.should have_selector('h1', :text => 'Home')
     end
   end
   
   describe "help page" do
-    it "should have the content 'Backlog Blooming | Help'" do
+    it "should have the right title" do
       visit '/info_pages/help'
-      page.should have_content('Backlog Blooming | Help')
+      page.should have_selector('title', :text => 'Backlog Blooming | Help')
+    end
+      
+    it "should have the right h1" do
+      visit '/info_pages/help'
+      page.should have_selector('h1', :text => 'Help')
     end
   end
   
   describe "about page" do
-    it "should have the content 'Backlog Blooming | About'" do
+    it "should have the right title" do
       visit '/info_pages/about'
-      page.should have_content('Backlog Blooming | About')
+      page.should have_selector('title', :text => 'Backlog Blooming | About')
+    end
+      
+    it "should have the right h1" do
+      visit '/info_pages/about'
+      page.should have_selector('h1', :text => 'About')
     end
   end
+  
 end
