9284a06ed6acce375fc2da697d8fa12bfa453c89
PunkWare <punkware@free.fr>
2012-03-11 23:14:09 +0100

    Replace Blueprint CSS by Bootstrap. Rewrite the custom CSS-Sass code completely.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,4 @@ BacklogBlooming::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
-  #Precompile Blueprint CSS
-  config.assets.precompile += [ "*blueprint*/*.css" ]
 end
