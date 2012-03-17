2a40b3479073ae43a018bef39824f1a1ec210fb6
PunkWare <punkware@free.fr>
2012-03-17 20:49:12 +0100

    A new user who sign up is now directly also signed in.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,17 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       flash[ :success ] = "Welcome and let's bloom !"
+      
+      #redirect_to user_path(@user)
+      
+      # Immediately sign in the new user
+      user = User.find_by_email(@user.email) 
+      if user && user.authenticate(@user.password)
+        sign_in user
+      end
+        
       redirect_to user_path(@user)
+        
     else
       render 'new'
     end
