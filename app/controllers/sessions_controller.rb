b31bd73139eec68bc8fd64120ae8fb937fb2a436
PunkWare <punkware@free.fr>
2012-03-17 20:24:47 +0100

    Add sign out page.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ def create
   user = User.find_by_email(params[:session][:email]) 
   if user && user.authenticate(params[:session][:password])
     sign_in user
-    redirect_to user
+    redirect_to user_path(user)
   else
     flash.now[:error] = 'Invalid email/password combination'
     render 'new'
@@ -15,6 +15,8 @@ def create
 end
 
 def destroy
+  sign_out
+  redirect_to root_path
 end
 
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -46,8 +46,14 @@ describe "Signin, and signout pages" do
       it { should have_link('Profile',    href: user_path(user)) }
       it { should have_link('Sign out',   href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
+      
+      describe "When clicking signout linking" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
     end
   end
+
   
 end
 
